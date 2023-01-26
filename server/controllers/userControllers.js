b09c42dc9a7fb9b30fc30a9547c2feaa9378b7bc
Diego Taveras <diegotaveras2@outlook.com>
2023-01-25 21:33:08 -0500

    hash password with bcrypt

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,17 +1,27 @@
 import Users from '../models/Users.js';
+import bcrypt from 'bcrypt';
+
 const newUser = async (req, res) => {
   // Verify is user is already registered
-  const { email } = req.body;
+  const { email, password } = req.body;
   let user = await Users.findOne({ email });
-
   if (user) {
     return res.status(400).json({ msg: 'User is already registered' });
   }
 
+  // Create new User
   user = await new Users(req.body);
-  user.save();
 
-  res.json({ msg: 'User Created Succesfully' });
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(password, salt);
+
+  try {
+    await user.save();
+    res.json({ msg: 'User Created Succesfully' });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { newUser };
