c9e9284afb6c75a915150b7b2bf648cc54ff2e17
Diego Taveras <diegotaveras2@outlook.com>
2023-01-26 12:33:53 -0500

    header component

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="py-8 flex felx-col md:flex-row item-center justify-between">
-      header
+      <img className="w-64 mb-8 md:mb-0" src="logo.png" alt="" />
     </header>
   );
 };
