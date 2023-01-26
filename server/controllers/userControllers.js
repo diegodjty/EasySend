6ca71104f1bf622f253ac43834c65bc7928aa8a9
Diego Taveras <diegotaveras2@outlook.com>
2023-01-25 21:27:36 -0500

    avoid duplicated users

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,9 +1,16 @@
 import Users from '../models/Users.js';
 const newUser = async (req, res) => {
-  console.log(req.body);
+  // Verify is user is already registered
+  const { email } = req.body;
+  let user = await Users.findOne({ email });
 
-  const user = await new Users(req.body);
+  if (user) {
+    return res.status(400).json({ msg: 'User is already registered' });
+  }
+
+  user = await new Users(req.body);
   user.save();
+
   res.json({ msg: 'User Created Succesfully' });
 };
 
